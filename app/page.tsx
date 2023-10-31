dbe20f70c22fdeb53e41571fbe4fa6fb389039d7
Subhampreet <subhampreet18@gmail.com>
2023-10-31 23:11:49 +0530

    Build Dummy Text : 01

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ export default function Home() {
   return (
     <main>
       <h1>Trello 2.0 Clone</h1>
+
+      <div className="mid">
+            <p>
+              {/* <span >I'm Freelance Web and</span> Product Developer Transforming{' '} */}
+              I'm Freelance Web and Product Developer Transforming {' '}
+              <img src="https://drive.google.com/uc?export=view&id=1cak6zhm5UH8A-2suT3FcVYwQ3Gg6ciAK" /> Code Into Art
+            </p>
+            {/*  */}
+            <p></p>
+      </div>
     </main>
   )
 }
